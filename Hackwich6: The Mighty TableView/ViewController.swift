3976d519fb4a854d1d48338f383fc171c24a68ed
KO-d14 <ainajk@hawaii.edu>
2023-10-12 23:56:03 -1000

    "Completed Parts" 1-8 REASONS FOR QUOTATION ON COMPLETED IS EXPLAINED BELOW:
    - NOTE: ran into error around/ starting line 35 for PART 7 #2 <-- tried cutting and pasting it back into code block, 2nd time around I pasted it on line 36 by accident but saw the error still being shown on line 35; emailed instructor regarding this issue; due to this, I was unable to run my code. However, I feel fairly confident in my lines of codes, especially the fact that I copy and pasted from the guidelines (only changed the name to "myClassmatesArray") for to suspect any other reasons why error is being thrown

--- Hackwich6: The Mighty TableView/ViewController.swift
+++ Hackwich6: The Mighty TableView/ViewController.swift
@@ -5,9 +5,41 @@
 //  Created by Aina Kodaira on 10/12/23.
 //
 
+//HACKWICH #6
+// PART 1: PROJECT SET UP
+// PART 2: WHAT IS UI TABLEVIEW?
+// PART 3: SET UP MAIN.STORYBOARD; ADD "TABLEVIEW" & "TABLEVIEWCELL" IN VC; CONNECT THE TWO TO VC THRU CONNECTIONS INSPECTOR
+// PART 4: WHAT ARE DATASOURCE & DELEGATES?
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+//HW#6 PART 8: SET UP PROTOYPE CELL
+    //PART 8 #2 - CREATE AN OUTLET
+    @IBOutlet weak var tableView: UITableView!
+    
+//HW#6 PART 6: CREATE AN ARRAY USING AN ARRAY LITERAL
+    // DECLARE AN ARRAY AND ADD 3 NAMES TO YOUR ARRAY
+    var myClassmatesArray = ["Sarah", "Nichole", "Grant"]
+//HW#6 PART 5: SET UP VIEWCONTROLLER.SWIFT FILE W/ DELEGATES & DELEGATES METHOD
+    // ADD THE PROTOCOLS
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //PART 5 #4
+       // return 1
+//HW#6 PART 7 : SET UP FOR DATASOURCE METHODS
+        // PART 7 #1 - SET NO. OF ROWS TO NO. OF ITEMS IN ARRAY
+        return myClassmatesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //PART 7 #2 - ADDED THE FOLLOWING BLOCK OF CODE
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let text = myClassmatesArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
